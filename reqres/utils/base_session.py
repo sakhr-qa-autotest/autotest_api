5672b0dd685cfac60680cae76ab0017c7104476e
sakhr-qa-autotest <salavat.kharasov@redlab.dev>
2023-04-13 00:58:47 +0400

    fix

--- reqres/utils/base_session.py
+++ reqres/utils/base_session.py
@@ -2,7 +2,7 @@ import logging
 
 from curlify import to_curl
 from requests import Session, Response
-
+from reqres.utils.allure_logger import allure_logger
 
 def request_logger(func):
     def wrapper(*args, **kwargs):
@@ -19,6 +19,8 @@ class BaseSession(Session):
         super(BaseSession, self).__init__()
         self.url = url
 
+    @allure_logger
+    @request_logger
     def request(self, method, url, **kwargs) -> Response:
         response = super().request(method, self.url + url, **kwargs)
         return response
